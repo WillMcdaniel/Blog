d84c2a7695f442e702f489e9386cf4f8f2f71751
WillMcdaniel <125214391+WillMcdaniel@users.noreply.github.com>
2023-10-11 11:42:00 -0700

    initial commit

---
+++ Blog1/blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+
+urlspattern = [
+    path("", views.starting_page, name ="starting_page"),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug:slug>, views.post_detail",
+         name="post-detail-page"),
+]
+
+
